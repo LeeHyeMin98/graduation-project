f78a078bc366bbf3adb5ef131e0f7b57a4ac6351
LeeHyeMin98 <68264402+LeeHyeMin98@users.noreply.github.com>
2020-12-30 18:49:27 +0900

    Assets Scripty

---
+++ Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class Timer : MonoBehaviour
+{
+    public Text timeText;
+    private float time;
+
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        timeText.text = "Time:" + Mathf.Round(time) +" second";
+
+    }
+
+    //void SetData()
+    //{
+    //    PlayerPrefs.SetInt("Time", int.Parse(timeText.text));
+    //    PlayerPrefs.SetInt("Coin", coin);
+    //    PlayerPrefs.Save();
+    //}
+
+    //public void save()
+    //{
+    //    PlayerPrefs.SetInt("Time", int.Parse(timeText.text));
+
+    //}
+    //public void Load()
+    //{
+    //    if (PlayerPrefs.HasKey("Name"))
+    //    {
+    //        timeText.text = PlayerPrefs.GetInt("Time").ToString();
+    //    }
+    //}
+}
---
+++ start.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+    }
+    public void StartButton()
+    {
+        Invoke("startgame", .2f);
+    }
+
+    void startgame()
+    {
+        Application.LoadLevel("play");
+    }
+}
+
